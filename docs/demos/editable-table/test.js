bb525d35bbfc07bed2201ca18aa1c583559d254a
vitalets <noginsk@rambler.ru>
2013-10-19 16:53:54 +0400

    test:editable table cancel test

--- docs/demos/editable-table/test.js
+++ docs/demos/editable-table/test.js
@@ -4,46 +4,51 @@ describe('editable-table', function() {
     browser().navigateTo(mainUrl);
   });
 
-  iit('should show form by `edit` button and save new values', function() {
-    var s = '[ng-controller="EditableTableCtrl"] > form ';
+  var s = '[ng-controller="EditableTableCtrl"] > form ';
 
-    //edit button initially shown, form initially hidden
-    function checkClosed() {
-      expect(element(s+'table span[editable-text]:visible').count()).toBeGreaterThan(2);
-      expect(element(s+'table span[editable-select]:visible').count()).toBeGreaterThan(5);
-      expect(element(s+'.btn-edit button:visible').count()).toBe(1);
-      expect(element(s+'.btn-form button:visible').count()).toBe(0);
-      expect(element(s+'input[type="text"]').count()).toBe(0);
-      expect(element(s+'select').count()).toBe(0);
-    }
-
-    //form shown in disabled state (loading)
-    function checkWaiting() {
-      expect(element(s+'table span[editable-text]:visible').count()).toBe(0);
-      expect(element(s+'table span[editable-select]:visible').count()).toBe(0);
-      expect(element(s+'.btn-edit button:visible').count()).toBe(0);
-      expect(element(s+'.btn-form button:visible:disabled').count()).toBe(3);
-      expect(element(s+'input[type="text"]:visible:disabled').count()).toBeGreaterThan(2);
-      expect(element(s+'select:visible:disabled').count()).toBeGreaterThan(5);
-    }
-
-    //form enabled when data loaded
-    function checkShown() {
-      expect(element(s+'table span[editable-text]:visible').count()).toBe(0);
-      expect(element(s+'table span[editable-select]:visible').count()).toBe(0);
-      expect(element(s+'.btn-edit button:visible').count()).toBe(0);
-      expect(element(s+'.btn-form button:visible:enabled').count()).toBe(3);
-      expect(element(s+'input[type="text"]:visible:enabled').count()).toBeGreaterThan(2);
-      expect(element(s+'select:visible:enabled').count()).toBeGreaterThan(5);
-    }
-
-    //check initial values
+  //form enabled when data loaded
+  function checkInitialValues() {
+    expect(element(s+'table tr').count()).toBe(4);
     expect(element(s+'table tr:eq(1) td:eq(0) span[editable-text]').text()).toMatch('user1');
-    expect(element(s+'table tr:eq(2) td:eq(0) span[editable-text]').text()).toMatch('user2');
-    expect(element(s+'table tr:eq(3) td:eq(0) span[editable-text]').text()).toMatch('user3');
     expect(element(s+'table tr:eq(1) td:eq(1) span[editable-select]').text()).toMatch('status2');
     expect(element(s+'table tr:eq(1) td:eq(2) span[editable-select]').text()).toMatch('admin');
+    expect(element(s+'table tr:eq(2) td:eq(0) span[editable-text]').text()).toMatch('user2');
+    expect(element(s+'table tr:eq(3) td:eq(0) span[editable-text]').text()).toMatch('user3');    
+  }
 
+  //edit button initially shown, form initially hidden
+  function checkClosed() {
+    expect(element(s+'table span[editable-text]:visible').count()).toBeGreaterThan(2);
+    expect(element(s+'table span[editable-select]:visible').count()).toBeGreaterThan(5);
+    expect(element(s+'.btn-edit button:visible').count()).toBe(1);
+    expect(element(s+'.btn-form button:visible').count()).toBe(0);
+    expect(element(s+'input[type="text"]').count()).toBe(0);
+    expect(element(s+'select').count()).toBe(0);
+  }
+
+  //form shown in disabled state (loading)
+  function checkWaiting() {
+    expect(element(s+'table span[editable-text]:visible').count()).toBe(0);
+    expect(element(s+'table span[editable-select]:visible').count()).toBe(0);
+    expect(element(s+'.btn-edit button:visible').count()).toBe(0);
+    expect(element(s+'.btn-form button:visible:disabled').count()).toBe(3);
+    expect(element(s+'input[type="text"]:visible:disabled').count()).toBeGreaterThan(2);
+    expect(element(s+'select:visible:disabled').count()).toBeGreaterThan(5);
+  }
+
+  //form enabled when data loaded
+  function checkShown() {
+    expect(element(s+'table span[editable-text]:visible').count()).toBe(0);
+    expect(element(s+'table span[editable-select]:visible').count()).toBe(0);
+    expect(element(s+'.btn-edit button:visible').count()).toBe(0);
+    expect(element(s+'.btn-form button:visible:enabled').count()).toBe(3);
+    expect(element(s+'input[type="text"]:visible:enabled').count()).toBeGreaterThan(2);
+    expect(element(s+'select:visible:enabled').count()).toBeGreaterThan(5);
+  }
+
+  it('should show form by `edit` button and save new values', function() {
+    //check initial values
+    checkInitialValues();
     checkClosed();
 
     //show form
@@ -96,6 +101,37 @@ describe('editable-table', function() {
     expect(element(s+'table tr:eq(3) td:eq(0) span[editable-text]').text()).toMatch('empty');    
   });
 
-  // todo: write test for cancel button
+
+  it('should be able to cancel all changes', function() {
+    //check initial values
+    checkInitialValues();
+    checkClosed();
+    //show form
+    element(s+'.btn-edit button').click();
+    sleep(delay);
+    checkShown();
+
+    //set correct values
+    using(s+'table tr:eq(2) td:eq(0)').input('$data').enter('awesome'); //user2: name = awesome
+    using(s+'table tr:eq(1) td:eq(1)').select('$data').option('3'); //user1: status = status4
+    using(s+'table tr:eq(1) td:eq(2)').select('$data').option('0'); //user1: group = user
+
+    //add 2 new rows
+    expect(element(s+'table tr').count()).toBe(4);
+    element(s+'.btn-form button.pull-right').click();
+    element(s+'.btn-form button.pull-right').click();
+    expect(element(s+'table tr').count()).toBe(6);
+
+    //delete row 3
+    element(s+'table tr:eq(3) td:eq(3) button').click();
+    expect(element(s+'table tr').count()).toBe(5);
+
+    //click cancel  
+    element(s+'.btn-form button[ng-click="tableform.$cancel()"]').click();
+
+    //check reset values
+    checkInitialValues();
+    checkClosed();    
+  });
 
 });
