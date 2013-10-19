96614c6a8ff4fc148179900af70530d187bb6894
vitalets <noginsk@rambler.ru>
2013-10-19 17:23:16 +0400

    src: ignore right button click

--- src/js/editable-element/controller.js
+++ src/js/editable-element/controller.js
@@ -15,6 +15,10 @@ angular.module('xeditable').factory('editableController', ['$q', '$document', 'e
 
   // bind click to body: cancel|submit editables
   $document.bind('click', function(e) {
+    // ignore right/middle button click
+    if (e.which !== 1) {
+      return;
+    }
 
     var toCancel = [];
     var toSubmit = [];
@@ -370,11 +374,16 @@ angular.module('xeditable').factory('editableController', ['$q', '$document', 'e
 
       // click - mark element as clicked to exclude in document click handler
       self.editorEl.bind('click', function(e) {
+        // ignore right/middle button click
+        if (e.which !== 1) {
+          return;
+        }
+
         self.clicked = true;
       });
     };
 
-    //setWaiting
+    // setWaiting
     self.setWaiting = function(value) {
       if (value) {
         //participate in waiting only if not disabled
