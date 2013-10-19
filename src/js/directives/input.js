ed8250c1b9a42fd417229f4bddcecbd23ece66ca
vitalets <noginsk@rambler.ru>
2013-10-19 18:04:41 +0400

    add html5 inputs

---
+++ src/js/directives/input.js
@@ -0,0 +1,37 @@
+/*
+Input types: text|email|tel|number|url|color|date|datetime|time|month|week
+*/
+
+(function() {
+
+  var types = 'text|email|tel|number|url|color|date|datetime|time|month|week'.split('|');
+
+  //todo: datalist
+  
+  // generate directives
+  angular.forEach(types, function(type) {
+    var directiveName = 'editable'+type.charAt(0).toUpperCase() + type.slice(1);
+    angular.module('xeditable').directive(directiveName, ['editableDirectiveFactory',
+      function(editableDirectiveFactory) {
+        return editableDirectiveFactory({
+          directiveName: directiveName,
+          inputTpl: '<input type="'+type+'">'
+        });
+    }]);
+  });
+
+  //`range` is bit specific
+  angular.module('xeditable').directive('editableRange', ['editableDirectiveFactory',
+    function(editableDirectiveFactory) {
+      return editableDirectiveFactory({
+        directiveName: 'editableRange',
+        inputTpl: '<input type="range" id="range" name="range">',
+        render: function() {
+          this.parent.render.call(this);
+          this.inputEl.after('<output>{{$data}}</output>');
+        }        
+      });
+  }]);
+
+}());
+
--- src/js/directives/text.js
+++
@@ -1,8 +0,0 @@
-//text
-angular.module('xeditable').directive('editableText', ['editableDirectiveFactory',
-  function(editableDirectiveFactory) {
-    return editableDirectiveFactory({
-      directiveName: 'editableText',
-      inputTpl: '<input type="text">'
-    });
-}]);
