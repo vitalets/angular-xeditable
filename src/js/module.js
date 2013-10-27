3556de527f77478f2c2af40abe54b3ef6e26ca5c
vitalets <noginsk@rambler.ru>
2013-10-27 13:43:57 +0400

    comments for defaults

--- src/js/module.js
+++ src/js/module.js
@@ -3,9 +3,36 @@ angular-xeditable module
 */
 angular.module('xeditable', [])
 .value('editableOptions', {
-  theme: 'default', //bs2, bs3,
+  /**
+   * Theme. Possible values `bs3`, `bs2`, `default`.
+   * 
+   * @var {string} theme
+   * @memberOf editable-options
+   */  
+  theme: 'default',
+  /**
+   * Whether to show buttons for single editalbe element.  
+   * Possible values `right` (default), `no`.
+   * 
+   * @var {string} buttons
+   * @memberOf editable-options
+   */    
   buttons: 'right',
+  /**
+   * Default value for `blur` attribute of single editable element.  
+   * Can be `cancel|submit|ignore`.
+   * 
+   * @var {string} blurElem
+   * @memberOf editable-options
+   */
   blurElem: 'cancel',
+  /**
+   * Default value for `blur` attribute of editable form.  
+   * Can be `cancel|submit|ignore`.
+   * 
+   * @var {string} blurForm
+   * @memberOf editable-options
+   */
   blurForm: 'ignore'
 });
 
