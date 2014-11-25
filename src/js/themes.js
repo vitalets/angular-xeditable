5d9234e4e7351602cce3a36d15baf4f71cfee882
Manuel Darveau <manueldarveau@gmail.com>
2014-11-24 23:33:38 -0500

    Added ```checkbox``` class to fix form checkbox alignment

--- src/js/themes.js
+++ src/js/themes.js
@@ -79,6 +79,8 @@ angular.module('xeditable').factory('editableThemes', function() {
               this.inputEl.addClass(this.theme.inputClass);
             }
           break;
+          case 'editableCheckbox':
+              this.editorEl.addClass('checkbox');
         }
 
         //apply buttonsClass (bs3 specific!)
