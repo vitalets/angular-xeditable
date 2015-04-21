f0490ee76a5e6deee05595cc19582b26009fc5ee
Alex Rea <git@area.io>
2015-04-21 17:31:16 +0100

    Dirty hack to avoid issue with textarea
    When the edit-disabled variable is set to an angular variable,
    an issue appears with a textarea, where self.parent.addListeners.call(self);
    is calling addListeners from the editableTextArea instance of
    the editableDirectiveFactory. This causes a loop, freezing
    the browser until it gives up when it runs out of stack.
    
    This is a dirty workaround, because I can't figure out how to fix
    the issue properly.

--- src/js/directives/textarea.js
+++ src/js/directives/textarea.js
@@ -6,7 +6,48 @@ angular.module('xeditable').directive('editableTextarea', ['editableDirectiveFac
       inputTpl: '<textarea></textarea>',
       addListeners: function() {
         var self = this;
-        self.parent.addListeners.call(self);
+        // self.parent.addListeners.call(self);
+        // The above line should be uncommented, and the copy/paste from it that I've put below
+        // is a great dirty hack, but I can't figure out the issue.
+        // self.parent.addListeners.call(self);
+        // submit textarea by ctrl+enter even with buttons
+        if (self.single && self.buttons !== 'no') {
+          self.autosubmit();
+        }
+        self.inputEl.bind('keyup', function(e) {
+          if (!self.single) {
+            return;
+          }
+
+          // todo: move this to editable-form!
+          switch (e.keyCode) {
+            // hide on `escape` press
+            case 27:
+              self.scope.$apply(function() {
+                self.scope.$form.$cancel();
+              });
+              break;
+          }
+        });
+
+        // autosubmit when `no buttons`
+        if (self.single && self.buttons === 'no') {
+          self.autosubmit();
+        }
+
+        // click - mark element as clicked to exclude in document click handler
+        // that closes all opened editables
+        self.editorEl.bind('click', function(e) {
+          // ignore right/middle button click
+          if (e.which !== 1) {
+            return;
+          }
+
+          if (self.scope.$form.$visible) {
+            self.scope.$form._clicked = true;
+          }
+        });
+
         // submit textarea by ctrl+enter even with buttons
         if (self.single && self.buttons !== 'no') {
           self.autosubmit();
