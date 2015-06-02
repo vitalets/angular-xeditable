abcb21bf6c60163fedf3ff20b5ff9cdf06e37691
Mitch Thompson <mitch@stti.org>
2015-06-02 16:38:39 -0400

    even simpler yet!

--- src/js/editable-element/directive.js
+++ src/js/editable-element/directive.js
@@ -33,12 +33,10 @@ function($parse, $compile, editableThemes, $rootScope, $document, editableContro
         // By default consider single element without any linked form.ß
         var hasForm = false;
      
-        var isSingle = attrs.eSingle !== undefined;
-
         // element wrapped by form
         if(ctrl[1]) {
           eFormCtrl = ctrl[1];
-          hasForm = !isSingle;
+          hasForm = attrs.eSingle === undefined;
         } else if(attrs.eForm) { // element not wrapped by <form>, but we hane `e-form` attr
           var getter = $parse(attrs.eForm)(scope);
           if(getter) { // form exists in scope (above), e.g. editable column
