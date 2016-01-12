22e9027de3bdb0fdf5752495b58b4806e2dd193b
Matthias Hennemeyer <mhennemeyer@me.com>
2016-01-12 13:21:44 +0100

    Add uib- prefixes to directives that depend on angular-ui-bootstrap

--- src/js/directives/bsdate.js
+++ src/js/directives/bsdate.js
@@ -18,10 +18,10 @@ angular.module('xeditable').directive('editableBsdate', ['editableDirectiveFacto
 				var buttonDatePicker = angular.element('<button type="button" class="btn btn-default"><i class="glyphicon glyphicon-calendar"></i></button>');
 				var buttonWrapper = angular.element('<span class="input-group-btn"></span>');
 
-				inputDatePicker.attr('datepicker-popup', this.attrs.eDatepickerPopupXEditable || 'yyyy/MM/dd' );
+				inputDatePicker.attr('uib-datepicker-popup', this.attrs.eDatepickerPopupXEditable || 'yyyy/MM/dd' );
 				inputDatePicker.attr('is-open', this.attrs.eIsOpen);
 				inputDatePicker.attr('date-disabled', this.attrs.eDateDisabled);
-				inputDatePicker.attr('datepicker-popup', this.attrs.eDatepickerPopup);
+				inputDatePicker.attr('uib-datepicker-popup', this.attrs.eDatepickerPopup);
 				inputDatePicker.attr('datepicker-mode', this.attrs.eDatepickerMode || 'day');
 				inputDatePicker.attr('min-date', this.attrs.eMinDate);
 				inputDatePicker.attr('max-date', this.attrs.eMaxDate);
