92db9bee79902b7fb80eecfcd080a6ea856a873b
Robert Perry <rlperry@lodestonetechnologies.com>
2016-10-06 22:40:03 -0400

    Modify the uiSelect directive to store the $matchElement and $choicesElement on the attributes. (note that $$element, which is also a jQuery wrapped element is already being stored on attrs) Add a previous and subsequent forms around the forms that contains the uiSelect to be tested and inserted a uiSelect into each of those forms have the same name, but different uiSelectChoices.

--- src/js/directives/uiselect.js
+++ src/js/directives/uiselect.js
@@ -4,25 +4,13 @@
  */
 angular.module('xeditable').directive('editableUiSelect',['editableDirectiveFactory', 'editableUtils',
     function(editableDirectiveFactory, editableUtils) {
-        var findElement = function(name) {
-            for(var i = 0, len = match.length; i < len; i++) {
-                  if (match[i].name === name) {
-                      return i;
-                  }
-              }
-        };
-
-        var match = [];
-        var choices = [];
-
         var dir = editableDirectiveFactory({
             directiveName: 'editableUiSelect',
             inputTpl: '<ui-select></ui-select>',
             render: function () {
-                var index = findElement(this.name);
                 this.parent.render.call(this);
-                this.inputEl.append(editableUtils.rename('ui-select-match', match[index].element));
-                this.inputEl.append(editableUtils.rename('ui-select-choices', choices[index].element));
+                this.inputEl.append(editableUtils.rename('ui-select-match', this.attrs.$matchElement));
+                this.inputEl.append(editableUtils.rename('ui-select-choices', this.attrs.$choicesElement));
                 this.inputEl.removeAttr('ng-model');
                 this.inputEl.attr('ng-model', '$parent.$parent.$data');
             }
@@ -34,8 +22,8 @@ angular.module('xeditable').directive('editableUiSelect',['editableDirectiveFact
             var matchEl = el.find('editable-ui-select-match');
             var choicesEl = el.find('editable-ui-select-choices');
 
-            match.push({name : attrs.name || attrs.editableUiSelect, element : matchEl.clone()});
-            choices.push({name : attrs.name || attrs.editableUiSelect, element : choicesEl.clone()});
+            attrs.$matchElement = matchEl.clone();
+            attrs.$choicesElement = choicesEl.clone();
 
             matchEl.remove();
             choicesEl.remove();
