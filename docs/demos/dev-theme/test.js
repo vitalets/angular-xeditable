f94a5375b139fd27adfe31e91bd173b3a568b21b
fredrikw <fredrik@wallner.nu>
2016-10-28 13:47:14 +0200

    Added possibility to set icon_set via attribute Also fixed icon_set value if theme is set via the attribute Tests added to dev-theme to test for both the fix and the enhancement Also made the dev-test run dev-theme and added some CSS to scripts-dev.jade to make the differences in themes apparent (only visible, the tests work the same with or without the css).

--- docs/demos/dev-theme/test.js
+++ docs/demos/dev-theme/test.js
@@ -32,5 +32,25 @@ describe('dev-theme', function() {
     expect(element(s+'form .editable-buttons button[type="submit"]').attr('class')).toBe(undefined);
   });
 
+  it('should have bs2 icons', function() {
+    var s = '[ng-controller="DevTheme"] ';
+    var a = s + 'a.bs2-test '
+
+    expect(element(a).text()).toMatch('awesome user');
+    element(a).click();
+
+    expect(element(s+'form .editable-buttons button[type="submit"] span').attr('class')).toBe('icon-ok icon-white')
+  });
+
+  it('should have font-awesome icons', function() {
+    var s = '[ng-controller="DevTheme"] ';
+    var a = s + 'a.fa-test '
+
+    expect(element(a).text()).toMatch('awesome user');
+    element(a).click();
+
+    expect(element(s+'form .editable-buttons button[type="submit"] span').attr('class')).toBe('fa fa-check')
+  });
+
 
 });
