e40cb6d26d7de5db0c8ff48272174d314e75a675
Ben Crinion <ben.crinion@gaist.co.uk>
2017-03-10 23:50:03 +0000

    Added different base urls for travis tests and local tests

--- protractor-conf.js
+++ protractor-conf.js
@@ -1,4 +1,10 @@
+var url = 'http://127.0.0.1:8000/'; // This should be local webserver
+if (process.env.TRAVIS) {
+	url = 'https://b3ncr.github.io/angular-xeditable/' // Change to your dev server
+}
+
 exports.config = {
+	baseUrl: url,
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	capabilities: {
 		'browserName': 'chrome'
