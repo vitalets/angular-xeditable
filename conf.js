4d37be413f107c0fac511c6a0fefb64715fa8427
Ben Crinion <ben.crinion@gaist.co.uk>
2017-03-24 00:38:15 +0000

    And another

--- conf.js
+++ conf.js
@@ -6,13 +6,13 @@ exports.config = {
 };
 
 if (process.env.TRAVIS) {
-	config.baseUrl = 'https://b3ncr.github.io/angular-xeditable/'; // Change to your dev server
+	exports.config.baseUrl = 'https://b3ncr.github.io/angular-xeditable/'; // Change to your dev server
 	exports.config.capabilities["tunnel-identifier"] = process.env.TRAVIS_JOB_NUMBER; // this is required by saucelabs
 	exports.config.sauceUser = process.env.SAUCE_USERNAME;
 	exports.config.sauceKey = process.env.SAUCE_ACCESS_KEY;
 }
 else{
-    config.baseUrl = 'http://127.0.0.1:8000/';
+    exports.config.baseUrl = 'http://127.0.0.1:8000/';
     exports.config.seleniumUrl = "http://localhost:4444/wd/hub";
 }
 
