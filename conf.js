3b214f4ff9ce0e431344cde04f12b69774269849
Ben Crinion <ben.crinion@gaist.co.uk>
2017-03-24 00:29:42 +0000

    Fixed url of test pages

--- conf.js
+++ conf.js
@@ -1,8 +1,4 @@
-var url = 'http://127.0.0.1:8000/'; // This should be local webserver
-
-
-exports.config = {
-	baseUrl: url,
+exports.config = {,
 	capabilities: {
 		"browserName": "chrome",
 	},
@@ -10,12 +6,13 @@ exports.config = {
 };
 
 if (process.env.TRAVIS) {
-	url = 'https://b3ncr.github.io/angular-xeditable/'; // Change to your dev server
+	config.baseUrl = 'https://b3ncr.github.io/angular-xeditable/'; // Change to your dev server
 	exports.config.capabilities["tunnel-identifier"] = process.env.TRAVIS_JOB_NUMBER; // this is required by saucelabs
 	exports.config.sauceUser = process.env.SAUCE_USERNAME;
 	exports.config.sauceKey = process.env.SAUCE_ACCESS_KEY;
 }
 else{
+    config.baseUrl = 'http://127.0.0.1:8000/';
     exports.config.seleniumUrl = "http://localhost:4444/wd/hub";
 }
 
