7742e5e1e565f188d4a2b1719c4bb4348c7590c5
Ben Crinion <ben.crinion@gaist.co.uk>
2017-03-24 00:02:21 +0000

    Try writing to console from protractor config

--- conf.js
+++ conf.js
@@ -1,10 +1,13 @@
+console.log("Protractor Config");
+console.log(process.env.SELENIUM_URL);
+
 var url = 'http://127.0.0.1:8000/'; // This should be local webserver
 var capabilities = {
     'browserName': 'firefox'
 };
 if (process.env.TRAVIS) {
     url = 'https://b3ncr.github.io/angular-xeditable/'; // Change to your dev server
-    capabilities["tunnel-identifier"] = process.env.TRAVIS_JOB_NUMBER; // this is required by saucelabs
+    capabilities.["tunnel-identifier"] = process.env.TRAVIS_JOB_NUMBER; // this is required by saucelabs
 }
 
 exports.config = {
