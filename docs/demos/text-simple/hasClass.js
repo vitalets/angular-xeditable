6858de0e48bf993f5ef5a7e773a4a19e6dedaff0
Ben Crinion <ben.crinion@gaist.co.uk>
2017-03-24 22:31:25 +0000

    added check for empty class

---
+++ docs/demos/text-simple/hasClass.js
@@ -0,0 +1,7 @@
+var hasClass = function (element, cls) {
+    return element.getAttribute('class').then(function (classes) {
+        return classes.split(' ').indexOf(cls) !== -1;
+    });
+};
+
+module.exports = hasClass;
