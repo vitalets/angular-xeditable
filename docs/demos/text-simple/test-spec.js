ba4e3e4cb2fd6870f82b5aa5536c0af5e9df7778
Ben Crinion <ben.crinion@gaist.co.uk>
2017-03-24 22:12:39 +0000

    added another test and migrated all existing tests to use page object

--- docs/demos/text-simple/test-spec.js
+++ docs/demos/text-simple/test-spec.js
@@ -6,7 +6,10 @@ describe('text-simple', function () {
     var editTextPage;
     
 	beforeEach(function () {
+        // create page object for each test
         editTextPage = new EditTextPage();
+        
+        // load the simple-text test view
 		browser.get(browser.baseUrl +'/docs/demos/text-simple/test-page.html');
 	});
 
@@ -16,18 +19,18 @@ describe('text-simple', function () {
 
 	it('should hide anchor after click', function () {
 		editTextPage.beginEdit();
-		expect(element(by.binding('user.name')).isDisplayed()).toBe(false);
+		expect(editTextPage.label.isDisplayed()).toBe(false);
 	});
 
 	it('should show input after click', function () {
 		editTextPage.beginEdit();
-		expect(element(by.model('$parent.$data')).isDisplayed()).toBe(true);
+		expect(editTextPage.input.isDisplayed()).toBe(true);
 	});
     
 	it('should contain the "awesome user" as initial text in input control', function () {
 		editTextPage.beginEdit();
 
-		var text = element(by.css(cssForOuterDiv + 'form input[type="text"]')).getAttribute('value');
+		var text = editTextPage.input.getAttribute('value');
 
 		expect(text).toBe("awesome user");
 	});
@@ -37,15 +40,19 @@ describe('text-simple', function () {
         editTextPage.beginEdit();
         
         // link is hidden
-		expect(element(by.binding('user.name')).isDisplayed()).toBe(false);
+        expect(editTextPage.label.isDisplayed()).toBe(false);
         
-        element(by.css('body')).click();
+        editTextPage.body.click();
        
-       // link is re-shown.
-		expect(element(by.binding('user.name')).isDisplayed()).toBe(true);
+        // link is re-shown.
+        expect(editTextPage.label.isDisplayed()).toBe(true);
 	});
     
     it('Should update model on button click', function(){
-    
+        editTextPage.setText("Mary");
+        
+        editTextPage.submitButton.click();
+
+        expect(editTextPage.label.getText()).toBe("Mary");
     });
 });
--- docs/demos/text-simple/textsimple.pageObject.js
+++ docs/demos/text-simple/textsimple.pageObject.js
@@ -1,6 +1,21 @@
-var page = function(){
-    this.beginEdit = function(){
-        element(by.binding('user.name')).click();
+var page = function () {
+    this.body = element(by.css('body'));
+    this.label = element(by.binding('user.name'));
+    this.input = element(by.model('$parent.$data'));
+    this.submitButton = element(by.css('button[type=submit]'));
+
+    this.beginEdit = function () {
+        this.label.click();
+    }
+
+    this.setText = function (text) {
+
+        let input1 = this.input;
+        this.label.click();
+
+        this.input.clear().then(function () {
+            input1.sendKeys(text);
+        });
     }
 };
 
