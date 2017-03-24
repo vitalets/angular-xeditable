c19ec2f27e4693443cc1a185abdd5a1bd11eeb06
Ben Crinion <ben.crinion@gaist.co.uk>
2017-03-24 00:25:54 +0000

    Try chrome browser capability and new config layout

--- conf.js
+++ conf.js
@@ -1,23 +1,21 @@
-console.log("Protractor Config");
-console.log(process.env.SELENIUM_URL);
-
 var url = 'http://127.0.0.1:8000/'; // This should be local webserver
-var capabilities = {
-    'browserName': 'firefox'
-};
-if (process.env.TRAVIS) {
-    url = 'https://b3ncr.github.io/angular-xeditable/'; // Change to your dev server
-    capabilities["tunnel-identifier"] = process.env.TRAVIS_JOB_NUMBER; // this is required by saucelabs
-}
+
 
 exports.config = {
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
-    baseUrl: url,
-    //seleniumAddress:   (process.env.SELENIUM_URL || 'http://localhost:4444/wd/hub'),
-    capabilities: {
-        "browserName": "firefox",
-        "tunnel-identifier": process.env.TRAVIS_JOB_NUMBER
-    },
-    specs: ['docs/demos/text-simple/test-spec.js']
+	baseUrl: url,
+	capabilities: {
+		"browserName": "chrome",
+	},
+	specs: ['docs/demos/text-simple/test-spec.js']
 };
+
+if (process.env.TRAVIS) {
+	url = 'https://b3ncr.github.io/angular-xeditable/'; // Change to your dev server
+	exports.config.capabilities["tunnel-identifier"] = process.env.TRAVIS_JOB_NUMBER; // this is required by saucelabs
+	exports.config.sauceUser = process.env.SAUCE_USERNAME;
+	exports.config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+}
+else{
+    exports.config.seleniumUrl = "http://localhost:4444/wd/hub";
+}
+
