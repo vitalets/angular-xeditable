dbb196b35a784c1733ce7fced8084a5ff4257460
Corey Kosloski <ckosloski@zipcar.com>
2017-05-23 17:58:30 -0500

    Added no buttons support for ui-select

--- docs/demos/uidate/test.js
+++ docs/demos/uidate/test.js
@@ -7,9 +7,29 @@ describe('uidate', function() {
   it('should show editor and submit new value', function() {
     var s = '[ng-controller="UidateCtrl"] ';
 
-    expect(element(s+'a[editable-uidate]').css('display')).not().toBe('none');
-    expect(element(s+'a[editable-uidate]').text()).toMatch('11/03/1985');
-    element(s+'a[editable-uidate]').click();
+    expect(element(s+'span[editable-uidate]').css('display')).not().toBe('none');
+    expect(element(s+'span[editable-uidate]').text()).toMatch('11/04/1985');
+    element(s+'span[editable-uidate]').click();
 
+    expect(element(s+'span[editable-uidate]').css('display')).toBe('none');
+    expect(element(s+'form[editable-form="$form"]').count()).toBe(1);
+    expect(element(s+'form input:visible').count()).toBe(1);
+    expect(element(s+'form input').val()).toBe('04/11/1985');
+    expect(element(s+'form .editable-buttons button[type="submit"]:visible').count()).toBe(1);
+    expect(element(s+'form .editable-buttons button[type="button"]:visible').count()).toBe(1);
+    expect(element('div#ui-datepicker-div:visible').count()).toBe(1);
+    expect(element('a.ui-state-active').text()).toMatch('11');
+
+    //set 29 april
+    element('div#ui-datepicker-div > table > tbody > tr:eq(0) > td:eq(1) > a').click();
+    expect(element(s+'div#ui-datepicker-div:visible').count()).toBe(0);
+    expect(element(s+'form input').val()).toBe('04/01/1985');
+
+    //submit
+    element(s+'form button[type="submit"]').click();
+
+    expect(element(s+'span[editable-uidate]').css('display')).not().toBe('none');
+    expect(element(s+'span[editable-uidate]').text()).toMatch('01/04/1985');
+    expect(element(s+'form').count()).toBe(0);
   });
 });
