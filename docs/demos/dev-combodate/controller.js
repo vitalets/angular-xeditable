844b3122d6309c8e1bd35f72aee2f136aabb6081
Corey Kosloski <ckosloski@zipcar.com>
2017-12-13 07:37:08 -0600

    Fix editable-combodate for single date fields

--- docs/demos/dev-combodate/controller.js
+++ docs/demos/dev-combodate/controller.js
@@ -3,4 +3,24 @@ app.controller('DevCombodateCtrl', function($scope) {
     dob: new Date(1984, 4, 15),
     dob2: new Date(1984, 4, 15, 10, 11, 0)
   };
+  $scope.myDate = {
+    date: new Date(1984, 4, 15),
+    format: "yyyy",
+    template: "YYYY"
+  };
+  $scope.myDate2 = {
+    date: new Date(1984, 4, 15),
+    format: "yyyy, MMMM",
+    template: "YYYY, MMMM"
+  };
+  $scope.myDate3 = {
+    date: new Date(1984, 4, 15),
+    format: "MMMM",
+    template: "MMMM"
+  }
+  $scope.myDate4 = {
+    date: new Date(1984, 4, 15),
+    format: "dd/MM/yyyy",
+    template: "DD/MM/YYYY"
+  };
 });
--- docs/demos/dev-combodate/test.js
+++ docs/demos/dev-combodate/test.js
@@ -37,12 +37,12 @@ describe('dev-combodate', function() {
     expect(element(s+'form').count()).toBe(0);
   });
 
-  it('should show editor and submit new value when initial date does not match avaiable values in drop downs', function() {
+  it('should show editor and submit new value when initial date does not match available values in drop downs', function() {
     var s = '[ng-controller="DevCombodateCtrl"] ';
 
-    expect(element(s+'a#minuteStep').css('display')).not().toBe('none');
-    expect(element(s+'a#minuteStep').text()).toMatch('May 15, 1984 10:11:00 AM');
-    element(s+'a#minuteStep').click();
+    expect(element(s+'span#minuteStep').css('display')).not().toBe('none');
+    expect(element(s+'span#minuteStep').text()).toMatch('May 15, 1984 10:11:00 AM');
+    element(s+'span#minuteStep').click();
 
     expect(element(s+'a#minuteStep').css('display')).toBe('none');
     expect(element(s+'form[editable-form="$form"]').count()).toBe(1);
@@ -58,8 +58,114 @@ describe('dev-combodate', function() {
     //submit
     element(s+'form button[type="submit"]').click();
 
-    expect(element(s+'a#minuteStep').css('display')).not().toBe('none');
-    expect(element(s+'a#minuteStep').text()).toMatch('May 15, 1984 10:10:00 AM');
+    expect(element(s+'span#minuteStep').css('display')).not().toBe('none');
+    expect(element(s+'span#minuteStep').text()).toMatch('May 1, 1984 10:10:00 AM');
+    expect(element(s+'form').count()).toBe(0);
+  });
+
+  it('should show editor for only year and submit new value', function() {
+    var s = '[ng-controller="DevCombodateCtrl"] ';
+
+    expect(element(s+'span#yearOnly').css('display')).not().toBe('none');
+    expect(element(s+'span#yearOnly').text()).toMatch('1984');
+    element(s+'span#yearOnly').click();
+
+    expect(element(s+'span#yearOnly').css('display')).toBe('none');
+    expect(element(s+'form[editable-form="$form"]').count()).toBe(1);
+    expect(element(s+'form select:visible').count()).toBe(1);
+    expect(element(s+'form select.year').val()).toBe('2017');
+    expect(element(s+'form .editable-buttons button[type="submit"]:visible').count()).toBe(1);
+    expect(element(s+'form .editable-buttons button[type="button"]:visible').count()).toBe(1);
+
+    //set 2017
+    element(s+'form select.year option[value=2017]').click();
+
+    //submit
+    element(s+'form button[type="submit"]').click();
+
+    expect(element(s+'span#yearOnly').css('display')).not().toBe('none');
+    expect(element(s+'span#yearOnly').text()).toMatch('2017');
+    expect(element(s+'form').count()).toBe(0);
+  });
+
+  it('should show editor for year and month only and submit new value', function() {
+    var s = '[ng-controller="DevCombodateCtrl"] ';
+
+    expect(element(s+'span#yearMonthOnly').css('display')).not().toBe('none');
+    expect(element(s+'span#yearMonthOnly').text()).toMatch('1984, May');
+    element(s+'span#yearMonthOnly').click();
+
+    expect(element(s+'span#yearMonthOnly').css('display')).toBe('none');
+    expect(element(s+'form[editable-form="$form"]').count()).toBe(1);
+    expect(element(s+'form select:visible').count()).toBe(2);
+    expect(element(s+'form select.year').val()).toBe('1984');
+    expect(element(s+'form select.month').val()).toBe('4');
+    expect(element(s+'form .editable-buttons button[type="submit"]:visible').count()).toBe(1);
+    expect(element(s+'form .editable-buttons button[type="button"]:visible').count()).toBe(1);
+
+    //set December 2017
+    element(s+'form select.month option[value=12]').click();
+    element(s+'form select.year option[value=2017]').click();
+      
+    //submit
+    element(s+'form button[type="submit"]').click();
+
+    expect(element(s+'span#yearMonthOnly').css('display')).not().toBe('none');
+    expect(element(s+'span#yearMonthOnly').text()).toMatch('2017, December');
+    expect(element(s+'form').count()).toBe(0);
+  });
+
+  it('should show editor for month only and submit new value', function() {
+    var s = '[ng-controller="DevCombodateCtrl"] ';
+
+    expect(element(s+'span#monthOnly').css('display')).not().toBe('none');
+    expect(element(s+'span#monthOnly').text()).toMatch('May');
+    element(s+'span#monthOnly').click();
+
+    expect(element(s+'span#monthOnly').css('display')).toBe('none');
+    expect(element(s+'form[editable-form="$form"]').count()).toBe(1);
+    expect(element(s+'form select:visible').count()).toBe(1);
+    expect(element(s+'form select.month').val()).toBe('4');
+    expect(element(s+'form .editable-buttons button[type="submit"]:visible').count()).toBe(1);
+    expect(element(s+'form .editable-buttons button[type="button"]:visible').count()).toBe(1);
+
+    //set December 2017
+    element(s+'form select.month option[value=12]').click();
+
+    //submit
+    element(s+'form button[type="submit"]').click();
+
+    expect(element(s+'span#monthOnly').css('display')).not().toBe('none');
+    expect(element(s+'span#monthOnly').text()).toMatch('December');
+    expect(element(s+'form').count()).toBe(0);
+  });
+
+  it('should show editor for day/month/year and submit new value', function() {
+    var s = '[ng-controller="DevCombodateCtrl"] ';
+
+    expect(element(s+'span#monthOnly').css('display')).not().toBe('none');
+    expect(element(s+'span#monthOnly').text()).toMatch('15/05/1984');
+    element(s+'span#monthOnly').click();
+
+    expect(element(s+'span#monthOnly').css('display')).toBe('none');
+    expect(element(s+'form[editable-form="$form"]').count()).toBe(1);
+    expect(element(s+'form select:visible').count()).toBe(3);
+      expect(element(s+'form select.day').val()).toBe('15');
+      expect(element(s+'form select.month').val()).toBe('4');
+      expect(element(s+'form select.year').val()).toBe('1984');
+    expect(element(s+'form .editable-buttons button[type="submit"]:visible').count()).toBe(1);
+    expect(element(s+'form .editable-buttons button[type="button"]:visible').count()).toBe(1);
+
+    //set December 25 2017
+    element(s+'form select.month option[value=12]').click();
+    element(s+'form select.day option[value=25]').click();
+    element(s+'form select.month option[value=11]').click(); // month option numbers appear to be offset by 1
+      
+    //submit
+    element(s+'form button[type="submit"]').click();
+
+    expect(element(s+'span#monthOnly').css('display')).not().toBe('none');
+    expect(element(s+'span#monthOnly').text()).toMatch('25/12/2017');
     expect(element(s+'form').count()).toBe(0);
   });
 });
