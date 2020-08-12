fa8e210d3f16c91b49550885f6cc5b853e2ffdca
Corey Kosloski <ckosloski@zipcar.com>
2020-08-12 17:14:26 -0500

    Allow name attribute on checklist

--- src/js/directives/checklist.js
+++ src/js/directives/checklist.js
@@ -9,10 +9,11 @@ angular.module('xeditable').directive('editableChecklist', [
       useCopy: true,
       render: function() {
         this.parent.render.call(this);
-        var parsed = editableNgOptionsParser(this.attrs.eNgOptions);
-        var ngChangeHtml = '';
-        var ngChecklistComparatorHtml = '';
-        var ngDisabled = '';
+        var parsed = editableNgOptionsParser(this.attrs.eNgOptions),
+            ngChangeHtml = '',
+            ngChecklistComparatorHtml = '',
+            ngDisabled = '',
+            ngNameHtml = '';
 
         if (this.attrs.eNgChange) {
           ngChangeHtml = ' ng-change="' +  this.attrs.eNgChange + '"';
@@ -22,19 +23,24 @@ angular.module('xeditable').directive('editableChecklist', [
           ngChecklistComparatorHtml = ' checklist-comparator="' +  this.attrs.eChecklistComparator + '"';
         }
 
-        if(this.attrs.eNgDisabled){
+        if (this.attrs.eNgDisabled) {
           ngDisabled = ' ng-disabled="' + this.attrs.eNgDisabled+'"';
         }
-        
+
+        if (this.attrs.eName) {
+          ngNameHtml = ' name="' +  this.attrs.eName + '"';
+        }
+
         var html = '<label ng-repeat="'+parsed.ngRepeat+'">'+
           '<input type="checkbox" checklist-model="$parent.$parent.$data" checklist-value="'+parsed.locals.valueFn+'"' +
-            ngChangeHtml + ngChecklistComparatorHtml + ngDisabled+'>'+
+            ngChangeHtml + ngChecklistComparatorHtml + ngNameHtml + ngDisabled + '>'+
           '<span ng-bind="'+parsed.locals.displayFn+'"></span></label>';
 
         this.inputEl.removeAttr('ng-model');
         this.inputEl.removeAttr('ng-options');
         this.inputEl.removeAttr('ng-change');
         this.inputEl.removeAttr('checklist-comparator');
+        this.inputEl.removeAttr('name');
         this.inputEl.html(html);
       }
     });
